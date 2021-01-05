e1a240323fd8abba9468424f2bc07aeaeabf3249
jo285317 <37353455+jo285317@users.noreply.github.com>
2021-01-05 15:48:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
 def  main():
     numbers = input()
     array = numbers.split(' ')
     _sum = 0
+    time.sleep(25)
     for num in array:
         _sum += int(num)
-    print(_sum)
+    print(_sum, end='')
 
 main()
